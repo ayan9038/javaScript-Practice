dce8159339c9d6fa9190fa2c8c95f16587726d42
Ayan Mondal <67552702+ayan9038@users.noreply.github.com>
2023-09-20 18:33:28 +0000

    variables in js

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const account_Id = 1836617
+let account_Email = "ayanmondal@google.com"
+var account_Password = "Ayan9038"
+account_City = "Barrackpur-I"
+account_Email = "ayan.mondal@netflix.com"
+account_Password = "Ayan90"
+account_City = "Silicon Valley"
+let account_State;
+// account_Id = 2 // not allowed
+console.log(account_Id);
+console.table([account_Id,account_Email,account_Password,account_City,account_State])
+/* prefer not to use var
+ because of issue in block scope and functional scope */
