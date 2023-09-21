c3571a12ffc6878e8b872cfaf64ba4e1f7cc6b06
Ayan Mondal <67552702+ayan9038@users.noreply.github.com>
2023-09-21 17:45:18 +0000

    conversion of a dataType

---
+++ basics/conversionOrOperation.js
@@ -0,0 +1,41 @@
+//let score = 33
+//let score = "33"
+//let score = "33abc"
+//let score = null
+//let score = undefined;
+//let score = true;\
+// let score = "Ayan"
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN -> Not a number ( but type Number)
+// true => 1 ; false => 0
+
+//let isLoggedIn = "Ayan"
+//let isLoggedIn = ""
+// let isLoggedIn = 1
+
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ayan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+ 
