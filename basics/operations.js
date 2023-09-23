bca7897493e31a55ad3eebae3ec8a71e95a8be31
Ayan Mondal <67552702+ayan9038@users.noreply.github.com>
2023-09-23 15:10:24 +0000

    operations

---
+++ basics/operations.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello"
+let str2 = " Ayan"
+
+let str3 = str1 + str2 
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+//console.log(true);
+//console.log(+true); // output = 1
+//console.log(+""); //output = 0
+
+let gameCounter = 100
+//gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
