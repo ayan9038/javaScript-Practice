53051f702fde29e6a8c308b0a165d6d647464fbd
Ayan Mondal <67552702+ayan9038@users.noreply.github.com>
2023-09-25 19:28:28 +0000

    comparison

---
+++ basics/comparision.js
@@ -0,0 +1,25 @@
+// confusion comparision, sp don't use
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // fasle
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// confusion comparision, sp don't use
+/*console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); // true
+*/
+
+// ===
+
+console.log("2" === 2);
+
+
